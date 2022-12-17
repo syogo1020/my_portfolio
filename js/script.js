2f1d194abcdbbc6d94bea47e14967da92b075941
しょうご <shogo@shougonoMacBook-Pro.local>
2022-12-17 12:54:49 +0900

    wordpress化初めテンプレートファイル

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 "use strict";
-
 var _$$slick;
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
